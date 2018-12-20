ce20d669386261cccd5496fed8a49e6cfcf8cde6
Andrew Villarrubia <andrew@primefour.com>
2018-12-19 19:45:34 -0600

    Update server.js to actually, you know, live on a server.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+'use strict';
+
 var express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path')
 var app = express();
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
@@ -21,4 +26,4 @@ app.get('/ipg', function(req, res) {
   res.render('pages/ipg');
 })
 
-app.listen(8080);
+app.listen(3000);
