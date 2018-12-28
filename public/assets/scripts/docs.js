8b43b04782bd44929194d1fd391b5c08a76f4030
Andrew Villarrubia <andrew@primefour.com>
2018-12-28 04:27:39 -0600

    Change style on doc pages.

--- public/assets/scripts/docs.js
+++ public/assets/scripts/docs.js
@@ -3,7 +3,7 @@ function findNewStuff() {
     if ($(this).find('.new').length !== 0 || $(this).hasClass('new-section')) {
       $(this).addClass('show');
     } else {
-      $(this).prev('h2').addClass('collapsed');
+      $(this).prev('h3').addClass('collapsed');
     }
   });
 }
@@ -11,7 +11,7 @@ function findNewStuff() {
 function linkAndExpand(id) {
   $(id).on('shown.bs.collapse', function() {
     console.log($(id).offset());
-    $('html').animate({scrollTop: $(id).prev('h2').offset().top}, 'fast');
+    $('html').animate({scrollTop: $(id).prev('h3').offset().top}, 'fast');
   });
   $(id).collapse('show');
 }
