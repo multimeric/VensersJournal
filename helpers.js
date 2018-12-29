fed4b65ed512248fc4b39f38b8daf5b569615cb7
Andrew Villarrubia <andrew@primefour.com>
2018-12-29 05:33:29 -0600

    Create helper functions to parse data to send to views.

---
+++ helpers.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var dateFormat = require('dateformat');
+var sets = require('./static/res/sets')
+
+module.exports = {
+  parseDocFiles: function(fileList) {
+    var docsArr = [];
+
+    for (var each of fileList.reverse()) {
+      var date = dateFormat(each.slice(4,-4), 'dd mmm, yyyy');
+      docsArr.push({filepath: each, date: date});
+    }
+
+    return docsArr;
+  },
+
+  parseCRFiles: function(fileList) {
+    var crArr = [];
+    var setDict = sets.getSetDict();
+
+    for (var each of fileList.reverse()) {
+      var date = dateFormat(each.slice(3,-8), 'dd mmm, yyyy');
+      crArr.push({filepath: each, setName: setDict[each.slice(-7,-4)]});
+    }
+    
+    return crArr;
+  }
+}
