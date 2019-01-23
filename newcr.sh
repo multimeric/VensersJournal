ad472d3767c69d393df815ee6bacb70530ba4afa
Andrew Villarrubia <andrew@primefour.com>
2019-01-22 18:07:51 -0600

    Create script to update site when new CR drops.

--- newcr.sh
+++ newcr.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-wget -O "$2" "$1";
-rsubl "$2"
-read -p "Waiting for the file to be saved..."
-python3 difftool/diff_rules.py "$4" "$5" "$2" "$3";
+# Arg list: Wizards media, new_path, new_setname, old_setname
+
+# the current string to the path on WotC media servers. Might change.
+wotc_prefix="http://media.wizards.com/2019/downloads/MagicCompRules "
+# path to rules folder
+rules_path="static/rules/"
+# path to diffs folder
+diffs="public/assets/cr-diffs"
+
+# Get the path for the previous CR
+old_path=$rules_path$(ls $rules_path | tail -1)
+# And also get the old diff
+old_diff=$(ls -t $diffs | head -n1 | cut -d '.' -f 1)
+
+# snag the new CR
+wget -O temp "$wotc_prefix$1" ;
+
+# convert the temp file to UTF-8 so we can actually use it
+iconv -f 850 -t UTF-8 -o "$rules_path$2" temp;
+# Dump the temp file
+rm temp
+# Diff 'em away boys
+python3 difftool/diff_rules.py $old_path "$4" "$rules_path$2" "$3";
+
+# ... now get the new diff
+new_diff=$(ls -t $diffs | head -n1 | cut -d '.' -f 1)
+
+sed -i -s "s/$old_diff/$new_diff/" views/partials/header.ejs
