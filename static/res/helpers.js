fd94926337403deb2abb246ce89011fff51e33a7
Andrew Villarrubia <andrew@primefour.com>
2019-01-26 03:03:44 -0600

    Fix IPG and MTR dates to actually be correct instead of a day behind what was expected. Javascript Date objects, man...

--- static/res/helpers.js
+++ static/res/helpers.js
@@ -7,8 +7,18 @@ module.exports = {
   parseDocFiles: function(fileList) {
     var docsArr = [];
 
+    /* Okay, buckle up, because this is some weird esoteric Javascript thing.
+     * If you try to get a Date object from a date that looks ISO-compliant
+     * (e.g. 2018-01-21), the Date object assumes that and will use UTC timezone.
+     * This means, for me, being in UTC -6:00, that all of my dates were a day off
+     * from what I expected them to be.
+     * If you pass in something less directly ISO-like, the Date formatter
+     * looses up its parser a bit and will use your actual timezone. So that's why
+     * I added the space there.
+     * Yes, I realize this comment is twice the length of the thing I'm commenting.
+     */
     for (var each of fileList.reverse()) {
-      var date = dateFormat(each.slice(4,-4), 'dd mmm, yyyy');
+      var date = dateFormat(each.slice(4,-4) + " ", 'dd mmm, yyyy');
       docsArr.push({filepath: each, date: date});
     }
 
@@ -20,8 +30,6 @@ module.exports = {
     var setDict = sets.getSetDict();
 
     for (var each of fileList.reverse()) {
-      var date = dateFormat(each.slice(3,-8), 'dd mmm, yyyy');
-      console.log(fileList, date);
       crArr.push({filepath: each, setName: setDict[each.slice(-7,-4)]});
     }
 
