8a8a968967347b14c4356c1cf21688661e8886fa
Andrew Villarrubia <andrew@primefour.com>
2019-10-04 20:06:42 -0500

    Make rule numbers on specific rule page links.

--- public/assets/scripts/rules.js
+++ public/assets/scripts/rules.js
@@ -1,5 +1,5 @@
 function findRules() {
-  $('td:odd').each(function() {
+  $('td:odd, p').each(function() {
     let link;
     if($(this).html().match(/\d{1,3}\.\w{1,4}/)) {
        link = $(this).html().replace(/\d{1,3}\.\w{1,4}/g, function(match) {
