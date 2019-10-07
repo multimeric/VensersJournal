c505fbb9831689c99a51075201bc84539948e7a0
Andrew Villarrubia <andrew@primefour.com>
2019-10-07 10:03:26 -0500

    Lol commented out code??? no ty

--- utils/update_card_list.py
+++ utils/update_card_list.py
@@ -70,31 +70,3 @@ with open(cardlist_location, 'w', encoding='utf-8') as out_cardlist:
             '"' + card + '": "' + card_image_dict[card] + '",\n')
     out_cardlist.write('}\n')
 print(datetime.now() - start)
-# req = requests.get(url=FULL_URL)
-# mtg_json_dump = req.json()
-
-# card_image_dict = {}
-# for name in mtg_json_dump['data']:
-#     this_url = CARD_URL + name
-#     req = requests.get(url=CARD_URL + name)
-#     try:
-#         card_image_dict[name] = req.json()['image_uris']['normal']
-#     except KeyError:
-#         dfc_image = req.json()['card_faces'][0]['image_uris']['normal']
-#         card_image_dict[name] = dfc_image
-#     except:
-#         print(name + " threw an unexpected error")
-#         continue
-
-# cardlist_location = '/home/vill/VensersJournal/static/res/cardlist.js'
-# with open(cardlist_location, 'w', encoding='utf-8') as out_cardlist:
-#     out_cardlist.write('''module.exports = {
-#                             getCardList: function() {
-#                             return cardList;
-#                             }
-#                         }\n''')
-#     out_cardlist.write('var cardList = {\n')
-#     for card in card_image_dict:
-#         out_cardlist.write(
-#             '"' + card + '": "' + card_image_dict[card] + '",\n')
-#     out_cardlist.write('}\n')
