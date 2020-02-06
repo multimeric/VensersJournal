625f146ee92607f72b8f3557d8b651444eff0a66
Andrew Villarrubia <andrew@primefour.com>
2020-02-06 16:40:58 -0600

    Add some more exceptions to what is or is not a 'real' card name.

--- public/assets/scripts/prettify.js
+++ public/assets/scripts/prettify.js
@@ -5,11 +5,20 @@ function makeItPretty() {
 function findCards() {
   $('p, li').each(function() {
     if($(this).html().match(/[A-Z][a-z]{2,}(?:(?:[ ,'\-](?:s| )? ?| \w{2,3}?(?:\s\w{2,3})? ?)[A-Z][a-z]*)*/)) {
+      // I am going to assume that anybody who plays this game knows what lands do.
       let problem_children = ["Plains", "Island", "Swamp", "Mountain", "Forest",
-                              "Snow-Covered Plains", "Snow-Covered Island", "Snow-Covered Swamp", "Snow-Covered Mountain", "Snow-Covered Forest"]
+                              "Snow-Covered Plains", "Snow-Covered Island", "Snow-Covered Swamp", "Snow-Covered Mountain", "Snow-Covered Forest",
+                              // Shows up in the IPG, unlikely to ever be actually referenced.
+                              "Warning",
+                              // Shows up as a card type, not a card name
+                              "Goblin Wizard"]
       let link = $(this).html().replace(/[A-Z][a-z]{2,}(?:(?:[ ,'\-](?:s| )? ?| \w{2,3}?(?:\s\w{2,3})? ?)[A-Z][a-z]*)*/g, function(match) {
         if (problem_children.includes(match)) {
           return match;
+        }
+        // Singular Elf in the IPG for clarity's sake. Grumble.
+        if (match === 'Llanowar Elf') {
+          return `<auto-card name="Llanowar Elves"">${match}</auto-card>`;
         }
         return `<auto-card>${match}</auto-card>`;
       });
